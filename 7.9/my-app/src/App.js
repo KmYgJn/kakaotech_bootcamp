a506464d034e9c2bbadadae66d502940fc3986a0
teddy.kim <qazkyj0310@gmail.com>
2024-07-10 16:26:42 +0900

    update: component login, signup, home

--- 7.9/my-app/src/App.js
+++ 7.9/my-app/src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import Signup from './signup/Signup';
+import Login from './login/Login';
+import Home from './home/Home';
+import {React, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <h1>HI, Hello World!</h1>
-    </div>
+      <Router>
+          <AppContent />
+      </Router>
+  );
+}
+
+function AppContent() {
+  const [showAlert, setShowAlert] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+      const params = new URLSearchParams(location.search);
+      if (params.get('signupSuccess')) {
+          setShowAlert(true);
+          setTimeout(() => {
+              setShowAlert(false);
+          }, 1000); // 1초 후에 알림창 사라짐
+      }
+  }, [location]);
+
+  return (
+      <section className="App">
+          {showAlert && (
+              <div className="alert-container">
+                  <div className="alert alert-success" role="alert">
+                      회원가입을 축하합니다!
+                  </div>
+              </div>
+          )}
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/home" element={<Home />} />
+          </Routes>
+      </section>
   );
 }
 
