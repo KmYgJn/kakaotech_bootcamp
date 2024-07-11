cfd230e9a9b6592dee85b6a60356cb130afa0421
teddy.kim <qazkyj0310@gmail.com>
2024-07-11 14:12:41 +0900

    add: home

---
+++ 7.11/my-app/src/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+
+
+const Home = () => {
+    return (
+        <section className="home">
+            <h3>Hi, This is your home.</h3>
+        </section>
+    )
+}
+
+export default Home
