d001f4d1bd6143b24f7eff242a4614ed79a2b9b0
teddy.kim <qazkyj0310@gmail.com>
2024-07-18 17:05:03 +0900

    add: oauth spring init project

---
+++ 7.17/test/src/test/java/com/qazyj/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qazyj.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
